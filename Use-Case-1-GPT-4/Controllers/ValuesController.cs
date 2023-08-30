a77d0b65d813f6c2640dac867b404463dccff84f
SOFTSERVE\nharash <nharash@softserveinc.com>
2023-08-30 12:38:37 +0300

    C#3 UC#1 Create endpoint

---
+++ Use-Case-1-GPT-4/Controllers/ValuesController.cs
@@ -0,0 +1,29 @@
+﻿using System.Diagnostics.Metrics;
+using System.Text.Json;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Use_Case_1_GPT_4.Models;
+
+namespace Use_Case_1_GPT_4.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private static readonly HttpClient httpClient = new();
+        [Route("task-1")]
+        [HttpPost]
+        public async Task<IActionResult> ReceiveValues([FromForm] FirstTaskModel model)
+        {
+            var response = await httpClient.GetAsync("https://restcountries.com/v3.1/all");
+
+            if (response.IsSuccessStatusCode)
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                var countries = JsonSerializer.Deserialize<List<object>>(content);
+                Console.WriteLine(content);
+            }
+            return Ok(true);
+        }
+    }
+}
---
+++ Use-Case-1-GPT-4/Models/FirstTaskModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Use_Case_1_GPT_4.Models
+{
+    public class FirstTaskModel
+    {
+        public string? Field1 { get; set; }
+        public string? Field2 { get; set; }
+        public int? Field3 { get; set; }
+        public int? Field4 { get; set; }
+    }
+}
