98c8556c1d1d5bcd4cb0c9a46989f788cc4e509b
SOFTSERVE\nharash <nharash@softserveinc.com>
2023-08-30 13:09:49 +0300

    C#5 UC#1 Create filter by population function

--- Use-Case-1-GPT-4/Controllers/ValuesController.cs
+++ Use-Case-1-GPT-4/Controllers/ValuesController.cs
@@ -30,6 +30,18 @@ namespace Use_Case_1_GPT_4.Controllers
             return Ok(filtered);
         }
 
+        [Route("by-population")]
+        [HttpGet]
+        public async Task<IActionResult> GetCountriesByPopulation([FromQuery] double population)
+        {
+            var countries = await this.GetCountries();
+            var filtered = countries
+                .Where(i => i.population != null 
+                        && i.population <= population * 100000);
+
+            return Ok(filtered);
+        }
+
         private async Task<List<Country>> GetCountries()
         {
             var response = await httpClient.GetAsync("https://restcountries.com/v3.1/all");
